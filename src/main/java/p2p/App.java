fe636b29ae05ce15e116910d95b8474bd1a247db
Shubham-Chauhan-2114 <shubhamchauhancode@gmail.com>
2025-07-27 21:30:20 +0530

    Comments added

--- src/main/java/p2p/App.java
+++ src/main/java/p2p/App.java
@@ -3,9 +3,6 @@ package p2p;
 import p2p.controller.FileController;
 import java.io.IOException;
 
-/**
- * PeerLink - P2P File Sharing Application
- */
 public class App {
     public static void main(String[] args) {
         try {
